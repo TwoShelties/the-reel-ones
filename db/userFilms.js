d5d644bcbd06697e92bdd1d999b7e7ad2b630d9b
ak6136 <107085714+ak6136@users.noreply.github.com>
2022-11-03 21:24:08 -0400

    userfilms.js

---
+++ db/userFilms.js
@@ -0,0 +1,42 @@
+const client = require(".");
+
+async function getFilmByUserId(userId) {
+  try {
+    const {
+      result: [film],
+    } = await client.query(
+      `
+            SELECT * FROM user_films 
+            WHERE "userId" = $1;
+            `,
+      [userId]
+    );
+    return film;
+  } catch (error) {
+    console.log("Error with getting film by userId");
+    throw error;
+  }
+}
+
+async function addFilmToUserId(userId, filmId, purchaseDate, expiryDate) {
+  try {
+    const {
+      result: [film],
+    } = await client.query(
+      `
+        INSERT INTO user_films (“userId”, “filmId”, "purchaseDate", "expiryDate")
+        VALUES ($1, $2, $3, $4) 
+        RETURNING *;
+        `,
+      [userId, filmId, purchaseDate, expiryDate]
+    );
+  } catch (error) {
+    console.log("Error with adding film to user profile");
+    throw error;
+  }
+}
+
+module.exports = {
+  getFilmByUserId,
+  addFilmToUserId,
+};
