1c8117c83f287c6df3792139a292a46a35c0a1c0
Roshni Nancy Chandrasekar <rnchandrasekar@email.wm.edu>
2022-11-10 18:41:39 -0500

    fixed login/register user errors

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // GET /api/books
-
+require("dotenv").config();
 const express = require("express");
 const apiRouter = require("./api");
-
 const app = express();
+
+const morgan = require("morgan");
+const cors = require("cors");
+
+// Setup your Middleware and API Router here
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
