dafe639b93097a8dec0d55ce03493361b7da1ec4
ak6136 <107085714+ak6136@users.noreply.github.com>
2022-11-10 21:21:07 -0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -12,7 +12,7 @@ const client = new Pool(
     : {
         // password: process.env.DB_PASSWORD,
         user: "postgres",
-        password: "rurufufu97",
+        password: "postgres",
         database: "the-reel-ones",
       }
 );
