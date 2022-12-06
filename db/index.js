5e1ad073bb9286b0afcc4e01299a4e3052f76193
TwoShelties <97826369+TwoShelties@users.noreply.github.com>
2022-12-05 20:53:27 -0500

    deployment3

--- db/index.js
+++ db/index.js
@@ -4,9 +4,7 @@ const { Pool } = require("pg");
 const client = new Pool(
   process.env.DATABASE_URL
     ? {
-        connectionString:
-          process.env.DATABASE_URL ||
-          "postgres://the_reel_ones_db_user:E9WbFtmIAF8XFGL3V4HK1Pj7k0XW1Nz0@dpg-ce76sasgqg4eemp7a1qg-a.ohio-postgres.render.com/the_reel_ones_db",
+        connectionString: process.env.DATABASE_URL,
         ssl: {
           rejectUnauthorized: false,
         },
